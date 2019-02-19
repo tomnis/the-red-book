2c38e6af9aed2e93e1d0a252126d5f1d25311c2f
tomas mccandless <tomas.mccandless@gmail.com>
2019-02-18 22:11:17 -0800

    chapter 2

---
+++ src/main/scala/exercises/Chapter2.scala
@@ -0,0 +1,39 @@
+package exercises
+
+import scala.annotation.tailrec
+
+trait Chapter2 {
+
+  def fib(n: Int): Int = {
+
+    @tailrec
+    def go(n: Int, fibn2: Int, fibn1: Int): Int = {
+      if (n <= 0) fibn2
+      else if (n <= 1) fibn1
+      else go(n - 1, fibn1, fibn1 + fibn2)
+    }
+
+    go(n, 0, 1)
+  }
+
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    val pairs = as.sliding(2)
+    pairs.forall(pair => ordered(pair.head, pair(1))) || pairs.forall(pair => !ordered(pair.head, pair(1)))
+  }
+  
+
+  def curry[A, B, C](f: (A, B) => C): A => B => C = {
+    a: A => (b: B) => f(a, b)
+  }
+
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
+    (a: A, b: B) => f(a)(b)
+  }
+
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C = {
+    a: A => f(g(a))
+  }
+}
---
+++ src/test/scala/exercises/Chapter2Spec.scala
@@ -0,0 +1,25 @@
+package exercises
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class Chapter2Spec extends FlatSpec with Matchers with Chapter2 {
+
+  "fib" should "work" in {
+    this.fib(0) should be (0)
+    this.fib(1) should be (1)
+    this.fib(5) should be (5)
+    this.fib(6) should be (8)
+  }
+
+  "isSorted" should "work" in {
+    val asc: Array[Int] = Array(1,2,3,3,4,5)
+    val desc: Array[Int] = asc.reverse
+    val ascOrder: (Int, Int) => Boolean = (a, b) => a <= b
+    val descOrder: (Int, Int) => Boolean = (a, b) => a >= b
+
+    this.isSorted(asc, ascOrder) should be (true)
+    this.isSorted(asc, descOrder) should be (false)
+    this.isSorted(desc, ascOrder) should be (false)
+    this.isSorted(desc, descOrder) should be (true)
+  }
+}
