63230930729aa2043f9804234a40069af163785b
Tomas McCandless <tomas.mccandless@gmail.com>
2019-04-15 23:02:18 -0700

    chapter 6

--- src/main/scala/exercises/State.scala
+++ src/main/scala/exercises/State.scala
@@ -35,17 +35,59 @@ object RNG {
       (f(a), rng2)
     }
 
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, s) = rng.nextInt
+    i match {
+      case Int.MinValue => (Int.MaxValue, s)
+      case other => (other, s)
+    }
+  }
 
-  def double(rng: RNG): (Double, RNG) = ???
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  // 6.2
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, s) = rng.nextInt
+    (i / Int.MaxValue.toDouble, s)
+  }
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  // 6.3
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, s) = rng.nextInt
+    val (d,s2) = double(s)
+    ((i, d), s2)
+  }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+    val (d, s) = double(rng)
+    val (i, s2) = s.nextInt
+    ((d, i), s2)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, s1) = double(rng)
+    val (d2, s2) = double(s1)
+    val (d3, s3) = double(s2)
+    ((d1,d2,d3), s3)
+  }
+
+
+  // 6.4
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    var state: RNG = rng
+    val nums = List.fill(count) {
+      val (i, s) = state.nextInt
+      state = s
+      i
+    }
+
+    (nums, state)
+  }
+
+  // 6.5
+  def elegantDouble: Rand[Double] = {
+    RNG.map(RNG.int)(i => i.toDouble / Int.MaxValue.toDouble)
+  }
 
   // combine 2 rng actions into one
   // 6.6
@@ -57,7 +99,18 @@ object RNG {
     }
   }
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+
+  // 6.7
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
+    rng => {
+      fs.foldRight((List.empty[A], rng)) { case (rand: Rand[A], (acc: List[A], oldState: RNG)) =>
+        val (i, newRng) =  rand(oldState)
+        (i :: acc, newRng)
+      }
+    }
+  }
+
+
 
   //6.8
   // generate a random A, then generate a random B based on that value
@@ -67,31 +120,58 @@ object RNG {
       g(a)(rng2)
     }
   }
+
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = rng => {
+    val (i, rng2) = nonNegativeInt(rng)
+    val mod = i % n
+    if (i + (n-1) - mod >= 0) (mod, rng2)
+      // TODO i think this should be rng2
+    else nonNegativeLessThan(n)(rng)
+  }
+
+
+  def nonNegativeLessThan2(n: Int): Rand[Int] = {
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if (i + (n-1) - mod >= 0) unit(mod)
+      else nonNegativeLessThan2(n)
+    }
+  }
+
+
+  // 6.9
+  def mapInTermsOfFlatMap[A,B](s: Rand[A])(f: A => B): Rand[B] = {
+    flatMap(s)(a => unit(f(a)))
+  }
+
+
+  def map2InTermsOfFlatMap[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    flatMap(ra)(a => map(rb)(b => f(a, b)))
+  }
+
+
+  def rollDie: Rand[Int] = map(nonNegativeLessThan(6))(_ + 1)
 }
 
 // why is state not contravariant?
 // is there any significant difference with run = (S => (S, A))?
-case class StateM[S,+A](run: S => (A, S)) {
+// 6.10
+case class State[S,+A](run: S => (A, S)) {
+  import State._
 
-  def map[B](f: A => B): StateM[S, B] = {
-    StateM[S, B] { oldState =>
-      val (a, newState) = run(oldState)
-      (f(a), newState)
-    }
+  def map[B](f: A => B): State[S, B] = {
+    flatMap(a => unit(f(a)))
   }
 
-  def map2[B,C](sb: StateM[S, B])(f: (A, B) => C): StateM[S, C] = {
-    StateM[S, C] { oldState =>
-      val (a, newState) = run(oldState)
-      val (b, newState2) = run(newState)
-      (f(a, b), newState2)
-    }
+  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] = {
+    flatMap(a => sb.map(b => f(a, b)))
   }
 
   // going with the analogy of flatMap = map andThen flatten,
   // run is like our "flatten"
-  def flatMap[B](f: A => StateM[S, B]): StateM[S, B] = {
-    StateM[S, B] { oldState =>
+  def flatMap[B](f: A => State[S, B]): State[S, B] = {
+    State[S, B] { oldState =>
       val (a, newState) = this.run(oldState)
       f(a).run(newState)
     }
@@ -99,16 +179,16 @@ case class StateM[S,+A](run: S => (A, S)) {
 }
 
 
-object StateM {
+object State {
 
-  def unit[S, A](a: A) = StateM(s => (a, s))
+  def unit[S, A](a: A): State[S, A] = State(s => (a, s))
 
-  def sequence[S, A](sas: List[StateM[S, A]]): StateM[S, List[A]] = {
-    StateM[S, List[A]] { oldState =>
-      sas.foldLeft((unit[S, List[A]](List.empty[A]), oldState)) { case ((acc: StateM[Nothing, List[A]], os), elem: StateM[S, A]) =>
-        val (i, newState) = elem.run(os)
-        (i :: acc, newState)
-      }
-    }
-  }
+//  def sequence[S, A](sas: List[State[S, A]]): State[S, List[A]] = {
+//    State[S, List[A]] { oldState =>
+//      sas.foldLeft((unit[S, List[A]](List.empty[A]), oldState)) { case ((acc: State[Nothing, List[A]], os), elem: State[S, A]) =>
+//        val (i, newState) = elem.run(os)
+//        (i :: acc, newState)
+//      }
+//    }
+//  }
 }
--- src/test/scala/exercises/Chapter6Spec.scala
+++ src/test/scala/exercises/Chapter6Spec.scala
@@ -1,20 +1,12 @@
 package exercises
 
 import exercises.RNG.{Rand, Simple}
+import RNG._
 
 /**
   * Created by tomas.mccandless on 2019-03-18.
   */
 class Chapter6Spec extends BaseSpec {
-
-
-  def nonNegativeInt(rng: RNG): (Int, RNG) = {
-    val (i, s) = rng.nextInt
-    i match {
-      case Int.MinValue => (Int.MaxValue, s)
-      case other => (other, s)
-    }
-  }
 
 
   "nonnegative" should "work" in {
@@ -23,66 +15,13 @@ class Chapter6Spec extends BaseSpec {
 
 
 
+  "map2" should "work" in {
+    val r1: Rand[Int] = RNG.int
+    val r2: Rand[Double] = RNG.elegantDouble
 
-  def double(rng: RNG): (Double, RNG) = {
-    val (i, s) = rng.nextInt
-    (i / Int.MaxValue.toDouble, s)
+    val intAndDouble: Rand[(Int, Double)] = RNG.map2(r1, r2)((a, b) => (a + 1, b + 2))
   }
 
-
-
-  def intDouble(rng: RNG): ((Int, Double), RNG) = {
-    val (i, s) = rng.nextInt
-    val (d,s2) = double(s)
-    ((i, d), s2)
-  }
-
-
-  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
-    val (d,s) = double(rng)
-    val (i, s2) = s.nextInt
-    ((d, i), s2)
-  }
-
-
-  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
-    val (d1, s1) = double(rng)
-    val (d2, s2) = double(s1)
-    val (d3, s3) = double(s2)
-    ((d1,d2,d3), s3)
-  }
-
-
-
-
-  // 6.4
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
-    var state: RNG = rng
-    val nums = List.fill(count) {
-      val (i, s) = state.nextInt
-      state = s
-      i
-    }
-
-    (nums, state)
-  }
-
-
-  // 6.5
-  def elegantDouble: Rand[Double] = {
-    RNG.map(RNG.int)(i => i.toDouble / Int.MaxValue.toDouble)
-  }
-
-
-  // 6.7
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
-    rng => {
-      fs.foldRight((List.empty[A], rng)) { case (rand: Rand[A], (acc: List[A], oldState: RNG)) =>
-        val (i, newRng) =  rand(oldState)
-        (i :: acc, newRng)
-      }
-    }
-  }
 
 
   "sequence" should "work" in {
@@ -93,5 +32,49 @@ class Chapter6Spec extends BaseSpec {
 
     val seq = sequence(rs)
     println(rs)
+  }
+
+  sealed trait Input
+  case object Coin extends Input
+  case object Turn extends Input
+
+  case class Machine(locked: Boolean, candies: Int, coins: Int) {
+    val unlocked: Boolean = !this.locked
+  }
+
+  def run(m: Machine, input: Input): Machine = {
+    (m, input) match {
+      // out of candy => ignore all inputs
+      case (m, _) if m.candies < 1 => m
+      // inserting coin into locked machine => machine is unlocked, increment coins
+      case (m, Coin) if m.locked => m.copy(locked = false, coins = m.coins + 1)
+      // turn knob on unlocked machine => dispense candy, machine becomes locked
+      case (m, Turn) if m.unlocked => m.copy(locked = true, candies = m.candies - 1)
+      // turning knob on locked machine => nothing
+      // inserting coin in unlocked machine => nothing
+      case _ => m
+    }
+  }
+
+  /**
+    *
+    * @param inputs
+    * @return number of coins and candies left in the machine
+    */
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+    State[Machine, (Int, Int)] { s =>
+
+
+    }
+  }
+
+  // 6.11
+  "candy dispenser" should "work" in {
+    // input types: coin, turn knob
+    // states: locked or unlocked
+    // tracks how many candies are left, how many coins it contains
+
+
+
   }
 }
